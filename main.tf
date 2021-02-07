46bf44118b383fbc0bc134d5dca072100f4f0c39
chrisgallivan <41701655+chrisgallivan@users.noreply.github.com>
2021-02-07 15:01:14 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,11 @@ terraform {
 }
 
 provider "azurerm" {
-    features {}
+   client_id = var.deployment_client_id
+   client_secret = var.deployment_client_secret
+   subscription_id= var.deployment_subscription_id
+   tenant_id= var.deployment_tenant_id
+   features {}
 }
 
 resource "azurerm_resource_group" "example" {
